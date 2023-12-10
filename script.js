ff3b772203e4c4a41d7e12128e4957c15b92593f
Georgina <georginatania@georginas-mbp.mynetworksettings.com>
2023-12-09 23:08:03 -0500

    fixed progress

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.getElementById('generateButton').addEventListener('click', async function() {
-    const selectedColor = document.getElementById('colorPicker').value; // Get the selected color
-    await fetchColorRange(selectedColor); // Fetch color range based on selected color
+document.getElementById('generateButton').addEventListener('click', function() {
+    console.log('Button clicked');
+    const selectedColor = document.getElementById('colorPicker').value;
+    fetchColorRange(selectedColor);
 });
 
-// Function to fetch color range from the API
 async function fetchColorRange(hexColor) {
     const apiUrl = 'https://colour-web-service.onrender.com/generate-palette';
     console.log("Fetching URL:", apiUrl);
@@ -11,36 +11,30 @@ async function fetchColorRange(hexColor) {
     try {
         const response = await fetch(apiUrl, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ color: hexColor.substring(1) }) // Send color without the '#' character
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ color: hexColor.substring(1) })
         });
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const data = await response.json(); // Parsing the JSON response
-        displayColorRange(data); // Displaying the color range
+        const data = await response.json();
+        displayColorRange(data);
     } catch (error) {
-        console.error('Error:', error); // Handling errors
+        console.error('Error:', error);
     }
 }
 
-// Function to display color range
 function displayColorRange(colorData) {
     const container = document.getElementById('colorRangeContainer');
-    container.innerHTML = ''; // Clear previous results
-
-    // Check if colorData.primary exists and is an object
+    container.innerHTML = '';
     if (colorData.primary && typeof colorData.primary === 'object') {
-        // Get the first 5 keys from the colorData.primary object and reverse the array
         Object.keys(colorData.primary).slice(0, 5).reverse().forEach(key => {
             const colorBox = document.createElement('div');
             colorBox.className = 'color-box';
-            colorBox.style.backgroundColor = colorData.primary[key]; // Set the background color using the value
-            container.appendChild(colorBox); // Append the color box to the container
+            colorBox.style.backgroundColor = colorData.primary[key];
+            container.appendChild(colorBox);
         });
     } else {
         console.error('Received data is not in the expected format:', colorData);
